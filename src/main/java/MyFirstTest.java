f950becdd05169702df921b0af3a6305929def6f
evgeniy-dp <evgeniy_dp@i.ua>
2017-05-24 18:37:40 +0300

    first test

--- src/main/java/MyFirstTest.java
+++ src/main/java/MyFirstTest.java
@@ -1,14 +1,11 @@
-import com.sun.jna.platform.win32.WinDef;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import javax.jws.WebResult;
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
@@ -31,18 +28,55 @@ public class MyFirstTest {
 
     @Test
     public void testOpenLink() throws InterruptedException {
+
+        /* Test1:
+        Open http://juliemr.github.io/protractor-demo/
+        Assert that page opened */
+
         driver.navigate().to("http://juliemr.github.io/protractor-demo/");
+//      Thread.sleep(10000);
+
+        String link1 = driver.findElement(By.xpath("/html/body/div/div/h3")).getText();
+        Assert.assertEquals(link1, "Super Calculator");
+        System.out.println("Test1: Page is opened");
+
+        /* Test2:
+        Enter value "1" to first field and second field
+        Assert that values are entered */
+
         driver.findElement(By.xpath("/html/body/div/div/form/input[1]")).sendKeys("1");
         driver.findElement(By.xpath("/html/body/div/div/form/input[2]")).sendKeys("1");
+
+        String value1 = driver.findElement(By.xpath("//input[@ng-model='first']")).getAttribute("value");
+        String value2 = driver.findElement(By.xpath("//input[@ng-model='second']")).getAttribute("value");
+        Assert.assertEquals(value1, "1");
+        Assert.assertEquals(value2, "1");
+        System.out.println("Test2: " + value1 + " + " + value2 + " values are entered");
+
+        /* Test3
+        Choose "+" in operator field
+        Assert that + is selected */
+
+        driver.findElement(By.xpath("/html/body/div/div/form/select/option[1]")).click();
+        String plus = driver.findElement(By.xpath("/html/body/div/div/form/select/option[1]")).getText();
+        Assert.assertEquals(plus, "+");
+        System.out.println("Test3: " + plus + " is selected");
+
+        /*Test4
+        Click Go button
+        Assert that current result(big number in the left) equals 2 */
         driver.findElement(By.xpath("//*[@id=\"gobutton\"]")).click();
-        Thread.sleep(5000);
-        String str = driver.findElement(By.xpath("/html/body/div/div/form/h2")).getText();
-        Assert.assertEquals(str, "2");
+        Thread.sleep(8000);
+
+        String result = driver.findElement(By.xpath("/html/body/div[1]/div/form/h2")).getText();
+        Assert.assertEquals(result, "2");
+        System.out.println("Test4: Current result equals " + result);
     }
 
     @AfterTest
-    public void exit() {
+    public void closeBrowser() {
         driver.close();
+        System.out.println("Browser is closed");
     }
 
 }
